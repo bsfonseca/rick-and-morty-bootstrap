ccf471c604cecfdf8fd7259793456e7806f60ecf
bsfonseca <bs.fonseca12@gmail.com>
2023-09-01 18:22:56 -0300

    Exibição da espécie e status do personagem

--- script.js
+++ script.js
@@ -10,14 +10,22 @@ listarPersonagens();
 const atualizarLista = (personagens) => {
   lista.innerHTML = "";
   for (let personagem of personagens) {
+    let vivo = "vivo";
+
+    if (personagem.status == "Dead") {
+      vivo = "morto";
+    } else if (personagem.status == "unknown") {
+      vivo = "desconhecido";
+    }
+
     const personagemHtml = `
     <div class="personagem col-12 col-sm-6">
         <div class="row">
         <div class="col-3 imagem-personagem"></div>
         <div class="col-9 conteudo">
             <h2>${personagem.name}</h2>
-            <p>Vivo - Dog</p>
-            <h3></h3>
+            <p>${personagem.species}</p>
+            <p  class="${vivo}" >${personagem.status}</p>
         </div>
         </div>
     </div>
