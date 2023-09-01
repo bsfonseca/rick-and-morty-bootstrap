88af4aba1e53aae097eee784afa750d77089e37d
bsfonseca <bs.fonseca12@gmail.com>
2023-09-01 17:38:11 -0300

    Listagem de personagens da Api do rick and Morty

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const lista = document.getElementById("lista");
+
+const listarPersonagens = async () => {
+  const result = await axios.get("https://rickandmortyapi.com/api/character");
+  console.log(result.data.results);
+};
+listarPersonagens();
